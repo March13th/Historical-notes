3b4faa7529a44412c7cd1921428d7ab949f30d98
djangogirls <djangogirls@.com>
2018-05-25 06:36:53 +0800

    20180525

---
+++ Code/python解析url/parsed_url.py
@@ -0,0 +1,42 @@
+def parsed_url(url):
+    """
+    解析 url 返回 (protocol host port path)
+    有的时候有的函数, 它本身就美不起来, 你要做的就是老老实实写
+    """
+    # 检查协议
+    protocol = 'http'
+    if url[:7] == 'http://':
+        u = url.split('://')[1]
+    elif url[:8] == 'https://':
+        protocol = 'https'
+        u = url.split('://')[1]
+    else:
+        # '://' 定位 然后取第一个 / 的位置来切片
+        u = url
+
+    # 检查默认 path
+    i = u.find('/')
+    if i == -1:
+        host = u
+        path = '/'
+    else:
+        host = u[:i]
+        path = u[i:]
+
+    # 检查端口
+    port_dict = {
+        'http': 80,
+        'https': 443,
+    }
+    # 默认端口
+    port = port_dict[protocol]
+    if host.find(':') != -1:
+        h = host.split(':')
+        host = h[0]
+        port = int(h[1])
+
+    return protocol, host, port, path
+
+
+protocol, host, port, path=parsed_url('g.cn')
+print('protocol:{},host:{},port:{},path:{}'.format(protocol, host, port, path))
